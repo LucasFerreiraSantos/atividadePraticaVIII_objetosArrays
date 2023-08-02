68cd70c625019e298efe5a986b82d970c589048e
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-02 19:48:02 -0300

    feito o exer1

--- exer1/index.js
+++ exer1/index.js
@@ -1,4 +1,8 @@
-
+const produtos = {
+  preco: '90',
+  produto: 'mouse'
+}
+console.log(produtos)
 
 // 1. Crie um objeto em JavaScript para colocar dois atributos de um
 // produto. Atribua o preço e descrição do produto com o valor “90” e a
