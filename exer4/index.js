8a364fc9e9aa2058617bfb1791f259ba930bf3ee
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-08-02 23:02:39 -0300

    feito o exer4

--- exer4/index.js
+++ exer4/index.js
@@ -9,14 +9,28 @@ do{
       salario += parseFloat(prompt('Qual o seu salário?'))
     }
   individuos.push({
-    nome: nome,
-    idade: idade,
-    trabalho: trabalho,
-    salario: salario
+    nome,
+    idade,
+    trabalho,
+    salario
   })
 }while(confirm('Deseja cadastrar um novo indivíduo?'))
 
-document.write(individuos)
+  for(const individuo of individuos){
+    if(!individuo.trabalho){
+      document.write(`<p><strong>Pessoas desempregadas:</strong></p>`)
+      document.write(`<p>Nome: ${individuo.nome}, idade: ${individuo.idade};</p>`)
+    } else if(individuo.trabalho && individuo.salario < 2500){
+      document.write(`<p><strong>Pessoas empregadas com salários menores que 2500:</strong></p>`)
+      document.write(`<p>Nome: ${individuo.nome}, idade: ${individuo.idade},  salário: ${individuo.salario.toFixed(2)};</p>`)
+    } else if(individuo.trabalho && individuo.salario >= 2500){
+      document.write(`<p><strong>Pessoas empregadas com salários maiores que 2500:</strong></p>`)
+      document.write(`<p>Nome: ${individuo.nome}, idade: ${individuo.idade},  salário: ${individuo.salario.toFixed(2)};</p>`)
+    }else{
+      document.write('NENHUM VALOR INFORMADO!!!')
+    }
+  }
+
 // 4. Crie um cadastro de pessoas onde o usuário informe o nome, idade
 // e se está trabalhando ou não, se a pessoa estiver trabalhando pergunte para ele o salário que está ganhando. Para cada pessoa cadastrada, pergunte ao usuário se ele deseja continuar
 // cadastrando ou não. No final, mostre as pessoas que estão
